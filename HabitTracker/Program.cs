069ba13a9b7087c3866c7515529f966215ddbd34
Elijah Reck <kcerhajile@gmail.com>
2024-05-18 00:34:55 -0400

    Added menu

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -17,6 +17,42 @@ bool deleteDB = Console.ReadLine() == "y";
 //Cleanup
 if(deleteDB) db.Delete();
 
+class Menu
+{
+    public int DisplayMainMenu()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Main Menu");
+        Console.WriteLine();
+        Console.WriteLine("Select from the following:");
+        Console.WriteLine();
+        bool validInput = true;
+        int menuChoice;
+        do
+        {
+            Console.WriteLine("1. Close Application");
+            Console.WriteLine("2. View All Records");
+            Console.WriteLine("3. Insert Record");
+            Console.WriteLine("4. Delete Record");
+            Console.WriteLine("5. Update Record");
+            if(int.TryParse(Console.ReadLine(), out menuChoice))
+            {
+                if(menuChoice > 5 || menuChoice < 1)
+                {
+                    validInput = false;
+                    Console.WriteLine("Error: Option outside range");
+                    Console.WriteLine();
+                }
+            } else
+            {
+                Console.WriteLine("Please use 1-5 to make your selection");
+                Console.WriteLine();
+            }
+        } while (validInput);
+
+        return menuChoice;
+    }
+}
 class DB
 {
     public void CreateNew()
