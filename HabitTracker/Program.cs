6d050953e9dd939aab673bd5bfa3339c28d29636
Elijah Reck <kcerhajile@gmail.com>
2024-05-17 22:44:14 -0400

    Added class DB to handle and simplify sqlite tasks

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,50 +1,86 @@
 ﻿using Microsoft.Data.Sqlite;
-File.Delete("tasks.db");
 
-if (!File.Exists("tasks.db"))
-using (var connection = new SqliteConnection("Data Source=tasks.db"))
+DB db = new DB("tasks");
+
+db.CreateNew();
+
+foreach (var line in db.GetAll())
 {
-    connection.Open();
-
-    var command = connection.CreateCommand();
-    command.CommandText =
-    @"
-        CREATE TABLE tasks (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL
-        );
-
-        INSERT INTO tasks
-        VALUES (1, 'Tim'),
-               (2, 'Tom'),
-               (3, 'Tum');
-    ";
-    command.ExecuteNonQuery();
+    Console.WriteLine(line);
 }
-
-using (var connection = new SqliteConnection("Data Source=tasks.db"))
-{
-    connection.Open();
-
-    var command = connection.CreateCommand();
-    command.CommandText =
-    @"
-        SELECT name FROM tasks;
-    ";
-
-    using(var reader = command.ExecuteReader())
-    {
-        while (reader.Read())
-        {
-            Console.WriteLine(reader.GetString(0));
-        }
-    }
-
-}
-SqliteConnection.ClearAllPools();
 
 Console.WriteLine("Delete database? y/n");
 bool deleteDB = Console.ReadLine() == "y";
 
 //Cleanup
-if(deleteDB) File.Delete("tasks.db");
+if(deleteDB) db.Delete();
+
+class DB
+{
+    public string fileName { get; set; }
+
+    public DB(string fileName)
+    {
+        this.fileName = fileName;
+    }
+    public void CreateNew()
+    {
+        if(!File.Exists(this.fileName + ".db")){
+            using (var connection = new SqliteConnection($"Data Source={this.fileName}.db"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                CREATE TABLE tasks (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL
+                );
+
+                INSERT INTO tasks
+                VALUES (1, 'Tim'),
+                       (2, 'Tom'),
+                       (3, 'Tum');
+            ";
+                command.ExecuteNonQuery();
+            }
+            Console.WriteLine($"Database {this.fileName} created successfully");
+        }
+        else
+        {
+            Console.WriteLine("Database already exists");
+        }
+    }
+    public void Delete()
+    {
+        SqliteConnection.ClearAllPools();
+        File.Delete(this.fileName + ".db");
+    }
+
+    public List<string> GetAll()
+    {
+        List<string> results = new();
+
+        using (var connection = new SqliteConnection($"Data Source={this.fileName}.db"))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                SELECT * FROM tasks
+            ";
+            //command.Parameters.AddWithValue("$table", this.fileName);
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    results.Add(reader.GetString(1));
+                }
+            }
+        }
+
+        return results;
+    }
+}
